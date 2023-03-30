5acc7f35ac423fa83f49d1d9050d6f8c7706a2a5
Farooq Zaidi <123715058+farooqNU@users.noreply.github.com>
2023-03-30 12:18:56 +0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+/**
+ * @defgroup   MAIN main
+ *
+ * @brief      This file implements main. Please Comment you code
+ *
+ * @author     Enter Your name and Student ID here.
+ * @date       2023
+ */
+
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * @brief      { function_description }
+ *
+ * @param[in]  argc  The count of arguments
+ * @param      argv  The arguments array
+ *
+ * @return     { description_of_the_return_value }
+ */
+int main(int argc, char const *argv[])
+{
+    // It is left up to you how you use shared memory and multiple processes 
+    // to solve the conway problem
+    return 0;
+}
